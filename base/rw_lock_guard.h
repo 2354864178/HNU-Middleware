604eddeac5b0a2e0994bbbff2850813cd70d5781
YSY_HP <ysy1023a@163.com>
2026-05-04 00:51:40 +0800

    读写锁的RAII守卫类

---
+++ base/rw_lock_guard.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include <unistd.h>
+#include <atomic>
+#include <condition_variable>
+#include <cstdint>
+#include <cstdlib>
+#include <iostream>
+#include <mutex>
+#include <thread>
+
+namespace hnu {
+namespace Middleware {
+namespace base {
+
+template <typename RWLock> class ReadLockGuard {
+    public:
+    explicit ReadLockGuard(RWLock& lock) : rw_lock_(lock) {
+        rw_lock_.ReadLock();
+    }
+
+    ~ReadLockGuard() {
+        rw_lock_.ReadUnlock();
+    }
+
+    private:
+    ReadLockGuard(const ReadLockGuard& other) = delete;
+    ReadLockGuard& operator=(const ReadLockGuard& other) = delete;
+    RWLock& rw_lock_;
+};
+
+template <typename RWLock> class WriteLockGuard {
+    public:
+    explicit WriteLockGuard(RWLock& lock) : rw_lock_(lock) {
+        rw_lock_.WriteLock();
+    }
+
+    ~WriteLockGuard() {
+        rw_lock_.WriteUnlock();
+    }
+
+    private:
+    WriteLockGuard(const WriteLockGuard& other) = delete;
+    WriteLockGuard& operator=(const WriteLockGuard& other) = delete;
+    RWLock& rw_lock_;
+};
+
+} // namespace base
+} // namespace Middleware
+} // namespace hnu
